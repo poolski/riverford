9043e0c3441f42586317e9247c901c00a041da6b
Kyrill <1942093+poolski@users.noreply.github.com>
2026-05-17 22:42:24 +0100

    chore: project config and entry point

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:3001"
+    }
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js"
+  }
+});
